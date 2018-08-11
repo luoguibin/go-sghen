1003ccc82dad9b08276bace89e3b638f39a14a59
Sghen <945654877@qq.com>
2018-08-11 18:03:47 +0800

    Init: the project

---
+++ controllers/peotryset.go
@@ -0,0 +1,171 @@
+package controllers
+
+import (
+	"SghenApi/models"
+	"encoding/json"
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/astaxie/beego"
+)
+
+// PeotrysetController operations for Peotryset
+type PeotrysetController struct {
+	beego.Controller
+}
+
+// URLMapping ...
+func (c *PeotrysetController) URLMapping() {
+	c.Mapping("Post", c.Post)
+	c.Mapping("GetOne", c.GetOne)
+	c.Mapping("GetAll", c.GetAll)
+	c.Mapping("Put", c.Put)
+	c.Mapping("Delete", c.Delete)
+}
+
+// Post ...
+// @Title Post
+// @Description create Peotryset
+// @Param	body		body 	models.Peotryset	true		"body for Peotryset content"
+// @Success 201 {int} models.Peotryset
+// @Failure 403 body is empty
+// @router / [post]
+func (c *PeotrysetController) Post() {
+	var v models.Peotryset
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+		if _, err := models.AddPeotryset(&v); err == nil {
+			c.Ctx.Output.SetStatus(201)
+			c.Data["json"] = v
+		} else {
+			c.Data["json"] = err.Error()
+		}
+	} else {
+		c.Data["json"] = err.Error()
+	}
+	c.ServeJSON()
+}
+
+// GetOne ...
+// @Title Get One
+// @Description get Peotryset by id
+// @Param	id		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.Peotryset
+// @Failure 403 :id is empty
+// @router /:id [get]
+func (c *PeotrysetController) GetOne() {
+	idStr := c.Ctx.Input.Param(":id")
+	id, _ := strconv.ParseInt(idStr, 10, 64)
+	v, err := models.GetPeotrysetById(id)
+	if err != nil {
+		c.Data["json"] = err.Error()
+	} else {
+		c.Data["json"] = v
+	}
+	c.ServeJSON()
+}
+
+// GetAll ...
+// @Title Get All
+// @Description get Peotryset
+// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
+// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
+// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
+// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
+// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
+// @Success 200 {object} models.Peotryset
+// @Failure 403
+// @router / [get]
+func (c *PeotrysetController) GetAll() {
+	var fields []string
+	var sortby []string
+	var order []string
+	var query = make(map[string]string)
+	var limit int64 = 10
+	var offset int64
+
+	// fields: col1,col2,entity.col3
+	if v := c.GetString("fields"); v != "" {
+		fields = strings.Split(v, ",")
+	}
+	// limit: 10 (default is 10)
+	if v, err := c.GetInt64("limit"); err == nil {
+		limit = v
+	}
+	// offset: 0 (default is 0)
+	if v, err := c.GetInt64("offset"); err == nil {
+		offset = v
+	}
+	// sortby: col1,col2
+	if v := c.GetString("sortby"); v != "" {
+		sortby = strings.Split(v, ",")
+	}
+	// order: desc,asc
+	if v := c.GetString("order"); v != "" {
+		order = strings.Split(v, ",")
+	}
+	// query: k:v,k:v
+	if v := c.GetString("query"); v != "" {
+		for _, cond := range strings.Split(v, ",") {
+			kv := strings.SplitN(cond, ":", 2)
+			if len(kv) != 2 {
+				c.Data["json"] = errors.New("Error: invalid query key/value pair")
+				c.ServeJSON()
+				return
+			}
+			k, v := kv[0], kv[1]
+			query[k] = v
+		}
+	}
+
+	l, err := models.GetAllPeotryset(query, fields, sortby, order, offset, limit)
+	if err != nil {
+		c.Data["json"] = err.Error()
+	} else {
+		c.Data["json"] = l
+	}
+	c.ServeJSON()
+}
+
+// Put ...
+// @Title Put
+// @Description update the Peotryset
+// @Param	id		path 	string	true		"The id you want to update"
+// @Param	body		body 	models.Peotryset	true		"body for Peotryset content"
+// @Success 200 {object} models.Peotryset
+// @Failure 403 :id is not int
+// @router /:id [put]
+func (c *PeotrysetController) Put() {
+	idStr := c.Ctx.Input.Param(":id")
+	id, _ := strconv.ParseInt(idStr, 10, 64)
+	v := models.Peotryset{Id: id}
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+		if err := models.UpdatePeotrysetById(&v); err == nil {
+			c.Data["json"] = "OK"
+		} else {
+			c.Data["json"] = err.Error()
+		}
+	} else {
+		c.Data["json"] = err.Error()
+	}
+	c.ServeJSON()
+}
+
+// Delete ...
+// @Title Delete
+// @Description delete the Peotryset
+// @Param	id		path 	string	true		"The id you want to delete"
+// @Success 200 {string} delete success!
+// @Failure 403 id is empty
+// @router /:id [delete]
+func (c *PeotrysetController) Delete() {
+	idStr := c.Ctx.Input.Param(":id")
+	id, _ := strconv.ParseInt(idStr, 10, 64)
+	if err := models.DeletePeotryset(id); err == nil {
+		c.Data["json"] = "OK"
+	} else {
+		c.Data["json"] = err.Error()
+	}
+	c.ServeJSON()
+}
---
+++ models/peotry.go
@@ -0,0 +1,158 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type Peotry struct {
+	Id       int64     `orm:"column(p_id);pk" json:"id"`
+	SId      int64     `orm:"column(s_id);null" json:"setId"`
+	// UId      *User     `orm:"column(u_id);rel(fk)" json:"userId"`
+	UId      int64     `orm:"column(u_id);" json:"userId"`
+	PTitle   string    `orm:"column(p_title);size(20);null" json:"title"`
+	PTime    time.Time `orm:"column(p_time);type(datetime);null" json:"time"`
+	PContent string    `orm:"column(p_content);null" json:"content"`
+	PEnd     string    `orm:"column(p_end);null" json:"end"`
+	PImages  string    `orm:"column(p_images);null" json:"images"`
+}
+
+func (t *Peotry) TableName() string {
+	return "peotry"
+}
+
+func init() {
+	orm.RegisterModel(new(Peotry))
+}
+
+// AddPeotry insert a new Peotry into database and returns
+// last inserted Id on success.
+func AddPeotry(m *Peotry) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetPeotryById retrieves Peotry by Id. Returns error if
+// Id doesn't exist
+func GetPeotryById(id int64) (v *Peotry, err error) {
+	o := orm.NewOrm()
+	v = &Peotry{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+// GetAllPeotry retrieves all Peotry matches certain condition. Returns empty list if
+// no records exist
+func GetAllPeotry(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(Peotry))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []Peotry
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdatePeotry updates Peotry by Id and returns error if
+// the record to be updated doesn't exist
+func UpdatePeotryById(m *Peotry) (err error) {
+	o := orm.NewOrm()
+	v := Peotry{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeletePeotry deletes Peotry by Id and returns error if
+// the record to be deleted doesn't exist
+func DeletePeotry(id int64) (err error) {
+	o := orm.NewOrm()
+	v := Peotry{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&Peotry{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
