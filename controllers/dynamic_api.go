047ff2163ee581cf8c80919f7b48b2df085f098d
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-01-08 11:05:11 +0800

    [+]动态api添加日期测试

--- controllers/dynamic_api.go
+++ controllers/dynamic_api.go
@@ -4,6 +4,8 @@ import (
 	"go-sghen/models"
 	"regexp"
 	"strings"
+
+	"fmt"
 )
 
 // DynamicAPIController 自定义接口控制器
@@ -145,10 +147,34 @@ func (c *DynamicAPIController) GetDynamicDataByPath() {
 						}
 					case "datas":
 						datas := c.GetString("datas", "")
-						r, _ := regexp.Compile("[0-9,]+")
+						r, _ := regexp.Compile("^[0-9,]+$")
 						if len(datas) > 0 && r.MatchString(datas) {
 							// datasStr := strings.Join(datas,  ",")
 							sqlStr = strings.Replace(sqlStr, "${datas}", datas, -1)
+						} else {
+							data[models.STR_CODE] = models.CODE_ERR
+							data[models.STR_MSG] = "操作失败"
+							c.respToJSON(data)
+							return
+						}
+					case "date0":
+						date0 := c.GetString("date0", "")
+						r, _ := regexp.Compile("^[0-9\\-:\\s]+$")
+						if len(date0) > 0 && r.MatchString(date0) {
+							// datasStr := strings.Join(datas,  ",")
+							sqlStr = strings.Replace(sqlStr, "${date0}", date0, -1)
+						} else {
+							data[models.STR_CODE] = models.CODE_ERR
+							data[models.STR_MSG] = "操作失败"
+							c.respToJSON(data)
+							return
+						}
+					case "date1":
+						date1 := c.GetString("date1", "")
+						r, _ := regexp.Compile("^[0-9\\-:\\s]+$")
+						if len(date1) > 0 && r.MatchString(date1) {
+							// datasStr := strings.Join(datas,  ",")
+							sqlStr = strings.Replace(sqlStr, "${date1}", date1, -1)
 						} else {
 							data[models.STR_CODE] = models.CODE_ERR
 							data[models.STR_MSG] = "操作失败"
